6f7490feca319f1e88eeebf2e6bb33aa104447f0
Chris Larsen <clarsendev@protonmail.com>
2019-04-13 17:40:48 -0400

    Apply Singleton pattern to Preferences

--- src/preferences.cr
+++ src/preferences.cr
@@ -1,10 +1,29 @@
 require "yaml"
 
 struct Preferences
-  @home_folder = "/Users/#{`whoami`.chomp}/"
+  @@instance : Preferences?
+  HOME_FOLDER = "/Users/#{`whoami`.chomp}/"
 
   YAML.mapping(
-    project_folder: {type: String, default: @home_folder + "workspace"},
+    project_folder: {type: String, default: HOME_FOLDER + "workspace"},
     editor: {type: String, default: "Atom"}
   )
+
+  def self.load : Preferences
+    if @@instance.nil?
+      from_yaml("")
+    else
+      @@instance.not_nil!
+    end
+  end
+
+  # Raises an Error to prevent cloning.
+  def clone
+    raise "Can't clone instance of singleton #{self.class}"
+  end
+
+  # Raises an Error to prevent duping.
+  def dup
+    raise "Can't dup instance of singleton #{self.class}"
+  end
 end
--- test/preferences_test.cr
+++ test/preferences_test.cr
@@ -6,10 +6,24 @@ class PreferencesTest < Minitest::Test
   def initialize(arg)
     super(arg)
 
-    @preferences = Preferences.from_yaml("")
+    @preferences = Preferences.load
   end
 
-  def test_default_folder_location
+  def test_raise_error_on_clone
+    error = assert_raises do
+      @preferences.clone
+    end
+    assert_equal "Can't clone instance of singleton Preferences", error.message
+  end
+
+  def test_raise_error_on_dup
+    error = assert_raises do
+      @preferences.dup
+    end
+    assert_equal "Can't dup instance of singleton Preferences", error.message
+  end
+
+  def test_default_project_folder_location
     whoami = `whoami`.chomp
     folder = "/Users/#{whoami}/workspace"
     assert_equal folder, @preferences.project_folder
